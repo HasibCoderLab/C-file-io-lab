399377c8f5319ff3832080d8fd3b5fd2b6518f0b
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-25 22:03:36 +0600

    best practice

--- file-io.c
+++ file-io.c
@@ -3,16 +3,36 @@
 //               * Topic: File IO
 // ===============================================
 
+// #include <stdio.h>
+// int main(){
+//     FILE *fptr;
+//     fptr = fopen("file.txt","r");
+//     fclose(fptr);
+//     return 0;
+// }
+
+
+//  Best Practice  |✨✨| 
+
 #include <stdio.h>
 int main(){
-    FILE *fptr;
-    fptr = fopen("file.txt","r");
-    fclose(fptr);
+    FILE *fptr = fopen("newFile.txt" , "r");
+    if (fptr == NULL)
+    {
+        printf("File dosesnt't exist\n");
+    }else{
+     fclose(fptr);
+    }
+    
     return 0;
 }
 
 
 // #include <stdio.h>
 // int main(){
-    // return 0;
+//     return 0;
 // }
+
+
+// 👉👉  🔹🔹  Question
+// 💥 ⚡ 🔥 ✨
