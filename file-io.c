950d14998ff1a1d662f87227574d9f963f1f0954
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-26 06:34:03 +0600

    write code for  'File reading mode with [int]' of file io in C

--- file-io.c
+++ file-io.c
@@ -41,20 +41,33 @@
 // }
 
 
-// |✨✨|  File reading mode  |✨✨| 
+// |✨✨|  File reading mode with char |✨✨| 
 
 
+// #include <stdio.h>
+// int main(){
+//     FILE *fptr ;
+//    fptr= fopen("file.txt" , "r");
+
+//     char ch ;
+//     fscanf(fptr , "%c", &ch) ;
+//     printf("%c\n" , ch);  // output=>     H
+//     fscanf(fptr , "%c" , &ch);
+//     printf("%c\n" ,ch) ; // output   => i;
+
+//     fclose(fptr);
+//     return 0;
+// }
+
+// |✨✨|  File reading mode with int |✨✨| 
 #include <stdio.h>
 int main(){
-    FILE *fptr ;
-   fptr= fopen("file.txt" , "r");
+    FILE *fptr;
+    fptr = fopen("file.txt" , "r");
 
-    char ch ;
-    fscanf(fptr , "%c", &ch) ;
-    printf("%c\n" , ch);  // output=>     H
-    fscanf(fptr , "%c" , &ch);
-    printf("%c\n" ,ch) ; // output   => i;
-
+    int num;
+    fscanf(fptr, "%d" , &num);
+    printf("%d\n" , num);
     fclose(fptr);
     return 0;
 }
