a3df943ca443c9e6ae84d965ca6c5078cc290b7b
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-26 08:35:49 +0600

    uncomment code

--- solve_problem.c
+++ solve_problem.c
@@ -5,85 +5,85 @@
 
 // 👉👉  🔹🔹  Question 1️⃣  make a  program to read  5 integers  from a file
 
-// #include <stdio.h>
-// int main(){
-//    FILE  *fptr ;
-//    fptr = fopen("file.txt" , "r");
+#include <stdio.h>
+int main(){
+   FILE  *fptr ;
+   fptr = fopen("file.txt" , "r");
 
-//    int n;
-//    fscanf(fptr , "%d" , &n);
-//    printf("%d\n" , n);
+   int n;
+   fscanf(fptr , "%d" , &n);
+   printf("%d\n" , n);
 
-//    fscanf(fptr , "%d" , &n);
-//    printf("%d\n" , n);
+   fscanf(fptr , "%d" , &n);
+   printf("%d\n" , n);
 
-//    fscanf(fptr , "%d" , &n);
-//    printf("%d\n" , n);
+   fscanf(fptr , "%d" , &n);
+   printf("%d\n" , n);
 
-//    fscanf(fptr , "%d" , &n);
-//    printf("%d\n" , n);
+   fscanf(fptr , "%d" , &n);
+   printf("%d\n" , n);
 
-//    fscanf(fptr , "%d" , &n);
-//    printf("%d\n" , n);
+   fscanf(fptr , "%d" , &n);
+   printf("%d\n" , n);
 
   
 
-//    fclose(fptr) ; 
-//     return 0 ;
-// }
+   fclose(fptr) ; 
+    return 0 ;
+}
 
 // 👉👉  🔹🔹  Question  2️⃣  Make a program  to inpur student  information frrom
 //  a user  & enter  it  to a file
 
 
-// #include <stdio.h>
-// int main(){
-//     FILE *fptr;
-//     fptr = fopen("student.txt" , "w");
+#include <stdio.h>
+int main(){
+    FILE *fptr;
+    fptr = fopen("student.txt" , "w");
 
-//     char name [50];
-//     int age;
-//     float cgpa;
+    char name [50];
+    int age;
+    float cgpa;
 
-//     printf("Enter name : ");
-//     scanf( "%s" , name);
+    printf("Enter name : ");
+    scanf( "%s" , name);
 
-//     printf("Enter age : ");
-//     scanf("%d" , &age);
+    printf("Enter age : ");
+    scanf("%d" , &age);
 
-//    printf("Enter cgpa : " );
-//    scanf("%f" , &cgpa);
+   printf("Enter cgpa : " );
+   scanf("%f" , &cgpa);
 
-//    fprintf(fptr ,"Student name : %s\t" , name);
-//    fprintf(fptr,"Student age : %d\t" , age);
-//    fprintf(fptr,"Student cgpa : %f\t" , cgpa);
+   fprintf(fptr ,"Student name : %s\t" , name);
+   fprintf(fptr,"Student age : %d\t" , age);
+   fprintf(fptr,"Student cgpa : %f\t" , cgpa);
 
 
-//     fclose(fptr);
-//     return 0;
-// }
+    fclose(fptr);
+    return 0;
+}
 
 
 // 👉👉  🔹🔹  Question  3️⃣   write a program to write a all odd numbers from 1 to n  in a file 
 
 
-// #include <stdio.h>
-// int main(){
-//     FILE *fptr ;
-//     fptr = fopen("oddNumber.txt" , "w");
-//     int n ;
-//     printf("Enter Number : ");
-//     scanf("%d" , &n);
-//     for(int i = 0; i<=n ; i++){
-//         if(i%2 !=0) {
-//         fprintf(fptr,"Odd number is => %d\n " , i);
-//         }
-//     }
+#include <stdio.h>
+int main(){
+    FILE *fptr ;
+    fptr = fopen("oddNumber.txt" , "w");
+    int n ;
+    printf("Enter Number : ");
+    scanf("%d" , &n);
+    for(int i = 0; i<=n ; i++){
+        if(i%2 !=0) {
+        fprintf(fptr,"Odd number is => %d\n " , i);
+        }
+    }
 
     
-//     fclose(fptr);
-//     return 0;
-// }
+    fclose(fptr);
+    return 0;
+}
 
 
 
@@ -99,24 +99,13 @@ int main(){
     int a ;
     fscanf(fptr , "%d" , &a);
     int b ;
-    fscanf(fptr , "%d" , &b);
+    fscan(fptr , "%d" , &b);
     fclose(fptr);
     fptr = fopen("sum.txt" , "w");
    fprintf(fptr ,"sum is %d" , a+b);
     fclose(fptr);
     return 0;
 }
-
- 
-// #include <stdio.h>
-// int main(){
-//     FILE *fptr ;
-//     fptr = fopen("oddNumber.txt" , "w");
-//     fclose(fptr);
-//     return 0;
-// }
-
-
 
 // #include <stdio.h>
 // int main(){
