be7f58bbcdc0c5e34c23d9ccccfb2ae3c8343df1
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-26 07:59:30 +0600

    solve 2nd  problem  of 'File IO' in C

--- solve_problem.c
+++ solve_problem.c
@@ -5,37 +5,70 @@
 
 // 👉👉  🔹🔹  Question 1️⃣  make a  program to read  5 integers  from a file
 
-#include <stdio.h>
-int main(){
-   FILE  *fptr ;
-   fptr = fopen("file.txt" , "r");
+// #include <stdio.h>
+// int main(){
+//    FILE  *fptr ;
+//    fptr = fopen("file.txt" , "r");
 
-   int n;
-   fscanf(fptr , "%d" , &n);
-   printf("%d\n" , n);
+//    int n;
+//    fscanf(fptr , "%d" , &n);
+//    printf("%d\n" , n);
 
-   fscanf(fptr , "%d" , &n);
-   printf("%d\n" , n);
+//    fscanf(fptr , "%d" , &n);
+//    printf("%d\n" , n);
 
-   fscanf(fptr , "%d" , &n);
-   printf("%d\n" , n);
+//    fscanf(fptr , "%d" , &n);
+//    printf("%d\n" , n);
 
-   fscanf(fptr , "%d" , &n);
-   printf("%d\n" , n);
+//    fscanf(fptr , "%d" , &n);
+//    printf("%d\n" , n);
 
-   fscanf(fptr , "%d" , &n);
-   printf("%d\n" , n);
+//    fscanf(fptr , "%d" , &n);
+//    printf("%d\n" , n);
 
   
 
-   fclose(fptr) ; 
-    return 0 ;
+//    fclose(fptr) ; 
+//     return 0 ;
+// }
+
+// 👉👉  🔹🔹  Question  2️⃣  Make a program  to inpur student  information frrom
+//  a user  & enter  it  to a file
+
+
+#include <stdio.h>
+int main(){
+    FILE *fptr;
+    fptr = fopen("student.txt" , "w");
+
+    char name [50];
+    int age;
+    float cgpa;
+
+    printf("Enter name : ");
+    scanf( "%s" , name);
+
+    printf("Enter age : ");
+    scanf("%d" , &age);
+
+   printf("Enter cgpa : " );
+   scanf("%f" , &cgpa);
+
+   fprintf(fptr ,"Student name : %s\t" , name);
+   fprintf(fptr,"Student age : %d\t" , age);
+   fprintf(fptr,"Student cgpa : %f\t" , cgpa);
+
+
+    fclose(fptr);
+    return 0;
 }
 
 
 
-
-
+// #include <stdio.h>
+// int main(){
+//     return 0;
+// }
 
 
 
